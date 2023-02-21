13206cf984c5db7f729eb4d59f5b40f37bcca847
Jessuir Cleydson <jessuircleydson@gmail.com>
2023-02-20 21:03:58 -0300

    adicionando campo de tentativa e lista de palavras tentadas. Mostrando palavra correta em verde

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,28 @@
     <img alt="Vue logo" src="../assets/logo.png" id="logo" />
     <div class="canvas-container">
       <div class="control-group">
-
-        <input type="color" v-model="canvasColor">
-         <div class="input-group">
-           <label for="lineWidth">Largura da Linha</label>
-           <input id="lineWidth" type="number" v-model="canvasLineWidth">
-         </div>
+        <input type="color" v-model="canvasColor" />
+        <div class="input-group">
+          <label for="lineWidth">Largura da Linha</label>
+          <input id="lineWidth" type="number" v-model="canvasLineWidth" />
+        </div>
       </div>
-      <canvas id="myCanvas" :width="canvasW" :height="canvasH" @mousemove="draw" @mousedown="beginDrawing" @mouseup="stopDrawing" />
+      <canvas
+        id="my-canvas"
+        :width="canvasW"
+        :height="canvasH"
+        @mousemove="draw"
+        @mousedown="beginDrawing"
+        @mouseup="stopDrawing"
+      />
+      <div class="chat-box-container">
+        <div class="chat-box">
+          <template v-for="(attempt, i) in attempties">
+            <p :class="{'win': attempt.win}" :key="i">{{ attempt.word }}</p>
+          </template>
+        </div>
+        <input type="text" v-model="attemptWord" @keyup.enter="checkAttempt" />
+      </div>
     </div>
   </div>
 </template>
@@ -24,11 +38,17 @@ export default {
       x: 0,
       y: 0,
       isDrawing: false,
-      canvasColor: 'black',
+      canvasColor: "black",
       canvasW: 0,
       canvasH: 0,
       canvasLineWidth: 1,
-    }
+      avancaProgresso: 0,
+      winWord: "papagaio",
+      attemptWord: "",
+      attempties: [],
+      chatBox: null,
+      win: false,
+    };
   },
   methods: {
     drawLine(x1, y1, x2, y2) {
@@ -42,8 +62,13 @@ export default {
       ctx.closePath();
     },
     draw(e) {
-      if(this.isDrawing) {
-        this.drawLine(this.x, this.y, Math.round(e.offsetX), Math.round(e.offsetY));
+      if (this.isDrawing) {
+        this.drawLine(
+          this.x,
+          this.y,
+          Math.round(e.offsetX),
+          Math.round(e.offsetY)
+        );
         this.x = Math.round(e.offsetX);
         this.y = Math.round(e.offsetY);
       }
@@ -55,21 +80,39 @@ export default {
     },
     stopDrawing(e) {
       if (this.isDrawing) {
-        this.drawLine(this.x, this.y, Math.round(e.offsetX), Math.round(e.offsetY));
+        this.drawLine(
+          this.x,
+          this.y,
+          Math.round(e.offsetX),
+          Math.round(e.offsetY)
+        );
         this.x = 0;
         this.y = 0;
         this.isDrawing = false;
       }
-    }
+    },
+    checkAttempt() {
+      if (this.attemptWord === this.winWord) {
+        this.attempties.push({word: this.attemptWord, win: true});
+      } else {
+        this.attempties.push({word: this.attemptWord, win: false});
+      }
+      this.attemptWord = '';
+      this.updateScroll();
+    },
+    updateScroll() {
+      this.chatBox.scrollTop = this.chatBox.scrollHeight - this.chatBox.clientHeight;
+    },
   },
   mounted() {
-    var c = document.querySelector("#myCanvas");
-    this.canvasW = window.innerWidth - 100;
-    this.canvasH = window.innerHeight - 200;
+    var c = document.querySelector("#my-canvas");
+    this.canvasW = 700;
+    this.canvasH = 500;
     var ctx = c.getContext("2d");
     this.canvas = ctx;
+    this.chatBox = document.querySelector(".chat-box");
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -79,11 +122,11 @@ export default {
   align-items: center;
 }
 #logo {
-    max-width: 50px;
+  max-width: 50px;
 }
 .canvas-container {
   padding: 20px;
-  .control-group{
+  .control-group {
     margin-bottom: 10px;
     display: flex;
     align-items: center;
@@ -96,7 +139,25 @@ export default {
     }
   }
 }
-#myCanvas {
+.chat-box-container{ 
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  .chat-box {
+    width: 200px;
+    height: 150px;
+    overflow: scroll;
+    border: 1px solid black;
+    text-align: left;
+    padding-inline: 15px;
+    padding-bottom: 25px;
+
+    .win {
+      color: green;
+    }
+  }
+}
+#my-canvas {
   border: 2px solid black;
 }
 </style>
